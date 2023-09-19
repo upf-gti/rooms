d2b88cc70763e4fde1c22ea603a548e79b57f1d8
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2023-09-19 18:18:50 +0200

    Added Edit tracking the controller

--- src/tools/sculpt/tool.cpp
+++ src/tools/sculpt/tool.cpp
@@ -17,7 +17,8 @@ bool Tool::update(float delta_time)
     edit_update_counter += delta_time;
 
     // Update edit position
-    edit_to_add.position = Input::get_controller_position(HAND_RIGHT) - glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 controller_pos_perfect = Input::get_controller_position(HAND_RIGHT) - glm::vec3(0.0f, 1.0f, 0.0f);
+    edit_to_add.position += (controller_pos_perfect - edit_to_add.position) * 0.50f;
     edit_to_add.rotation = glm::inverse(Input::get_controller_rotation(HAND_RIGHT));
 
     // Update common edit dimensions
