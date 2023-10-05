09de9ca1eb4fdce533b80ec295b8ab110701d1fa
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2023-10-05 13:18:37 +0200

    Added parabola segmentation functions

--- src/tools/sculpt/sweep.cpp
+++ src/tools/sculpt/sweep.cpp
@@ -12,12 +12,79 @@ void SweepTool::initialize()
     Tool::initialize();
 
     renderer = dynamic_cast<RoomsRenderer*>(Renderer::instance);
+
+    arc_length_LUT = new float[STARTING_ARC_LENGTH];
+    arc_length_LUT_storeage_size = STARTING_ARC_LENGTH;
+
+    fill_edits_with_arc();
 }
 
 void SweepTool::clean()
 {
 
 }
+
+
+// Curve functions =============
+inline float SweepTool::sParametricParabola::x(const float t) const {
+    return -(curve_segment_size_pow_2) / (2 * curve_height) * t;
+}
+inline float SweepTool::sParametricParabola::y(const float t) const {
+    return -curve_segment_size_pow_2 / (4.0 * curve_height) * t * t + curve_height;
+}
+
+inline void SweepTool::fill_arc_length_LUT(const uint32_t element_count) {
+    const float step = 1.0f / (element_count * 0.25f);
+    float current_length = 0.0f, prev_x = curve.x(0.0f), prev_y = curve.y(0.0f);
+    arc_length_LUT_size = 0u;
+
+    for (float i = 0.0f; i < element_count; i++) {
+        const float curr_x = curve.x(i * step);
+        const float curr_y = curve.y(i * step);
+
+        const float delta_x = prev_x - curr_x;
+        const float delta_y = prev_y - curr_y;
+
+        current_length += sqrt(delta_x * delta_x + delta_y * delta_y);
+
+        arc_length_LUT[arc_length_LUT_size++] = current_length;
+
+        prev_x = curr_x;
+        prev_y = curr_y;
+    }
+}
+
+uint32_t SweepTool::get_closest_arc_length(const float length) const {
+    uint32_t i = 0u;
+
+    for (; i < arc_length_LUT_size; i++) {
+        if (arc_length_LUT[i] > length) {
+            return i-1u;
+        }
+    }
+    assert(false); // Should never happen: could not find a bigget length than the target
+    return 0u;
+}
+
+
+inline float SweepTool::aprox_inverse_curve_length(const float length) const {
+    const float target_length = length * arc_length_LUT[arc_length_LUT_size-1u];
+
+    const uint32_t closest_index = get_closest_arc_length(target_length);
+
+    const float clossest_length = arc_length_LUT[closest_index];
+
+    return (closest_index + (target_length - clossest_length) / (arc_length_LUT[closest_index + 1] - clossest_length)) / arc_length_LUT_size;
+}
+
+void SweepTool::fill_edits_with_arc() {
+    curve.set_parabola(5.0, 1.0);
+
+    fill_arc_length_LUT(25u);
+
+    std::cout << aprox_inverse_curve_length(0.0f) << "  " << aprox_inverse_curve_length(2.0 / 25.0) << "  " << aprox_inverse_curve_length(22.0 / 25.0) << std::endl;
+}
+
 
 uint32_t SweepTool::get_number_of_edits_in_stroke(const glm::vec3 &stroke_end_position, const glm::quat &stroke_end_orientation) const {
     return (uint32_t) glm::ceil(glm::length(stroke_start_position - stroke_end_position) / inter_edit_distance);
--- src/tools/sculpt/sweep.h
+++ src/tools/sculpt/sweep.h
@@ -2,6 +2,9 @@
 
 #include "tool.h"
 
+#define STARTING_ARC_LENGTH 100u
+#define ARC_LENGTH_INCREASE 50u
+
 class RoomsRenderer;
 
 enum eSweepStrokeState : uint8_t {
@@ -27,6 +30,29 @@ class SweepTool : public Tool {
 
     std::vector<Edit>   tmp_edit_storage;
 
+    // Curve data
+    float   *arc_length_LUT = nullptr;
+    uint32_t arc_length_LUT_storeage_size = 0u;
+    uint32_t arc_length_LUT_size = 0u;
+
+    struct sParametricParabola {
+        float curve_height = 0.0f;
+        float curve_segment_size_pow_2 = 0.0f;
+
+        float x(const float t) const;
+        float y(const float t) const;
+
+        inline void set_parabola(const float height, const float segment_size) {
+            curve_height = height;
+            curve_segment_size_pow_2 = segment_size * segment_size;
+        }
+    } curve;
+
+
+    void fill_arc_length_LUT(const uint32_t element_count);
+    uint32_t get_closest_arc_length(const float length) const;
+    float aprox_inverse_curve_length(const float length) const;
+    void fill_edits_with_arc();
 
     uint32_t get_number_of_edits_in_stroke(const glm::vec3& stroke_end_position, const glm::quat& stroke_end_orientation) const;
 
