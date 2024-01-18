8f38b279c95eb2f287c7eca622251c57cac304f5
Alex Rodríguez <alejandro.rodriguez@upf.edu>
2024-01-18 13:47:43 +0100

    No textures for alpha mask, use distances

--- src/graphics/renderers/mesh_renderer.cpp
+++ src/graphics/renderers/mesh_renderer.cpp
@@ -74,6 +74,7 @@ void MeshRenderer::init_render_mesh_pipelines()
     Pipeline::register_render_pipeline(RendererStorage::get_shader("data/shaders/ui/ui_group.wgsl"), color_target);
     Pipeline::register_render_pipeline(RendererStorage::get_shader("data/shaders/ui/ui_button.wgsl"), color_target);
     Pipeline::register_render_pipeline(RendererStorage::get_shader("data/shaders/ui/ui_slider.wgsl"), color_target);
+    Pipeline::register_render_pipeline(RendererStorage::get_shader("data/shaders/ui/ui_slider_h.wgsl"), color_target);
     Pipeline::register_render_pipeline(RendererStorage::get_shader("data/shaders/ui/ui_color_picker.wgsl"), color_target);
     Pipeline::register_render_pipeline(RendererStorage::get_shader("data/shaders/sdf_fonts.wgsl"), color_target);
 }
