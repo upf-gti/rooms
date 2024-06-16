13122169f8cee949958dc0790d8950ff78d7411e
jxarco <alexroco.30@gmail.com>
2024-06-16 05:09:28 +0200

    improve keyboard

--- src/framework/ui/keyboard.cpp
+++ src/framework/ui/keyboard.cpp
@@ -14,24 +14,36 @@ namespace ui {
     Viewport3D* Keyboard::xr_keyboard = nullptr;
     XrKeyboardState Keyboard::state;
     glm::vec2 Keyboard::keyboard_size = {};
+    ui::Text2D* Keyboard::caret = nullptr;
+
     bool Keyboard::active = false;
+    float Keyboard::input_start_position = 0.0f;
+
+    ui::XRPanel* Keyboard::root_common = nullptr;
+    ui::XRPanel* Keyboard::root_lc = nullptr;
+    ui::XRPanel* Keyboard::root_uc = nullptr;
+    ui::XRPanel* Keyboard::root_sym = nullptr;
 
     void XrKeyboardState::set_input(const std::string& str)
     {
         input = ":" + str;
         text->set_text(input);
+        p_caret = input.size();
     }
 
     void XrKeyboardState::clear_input()
     {
         input = ":";
         text->set_text(input);
+        p_caret = 1u;
     }
 
     void XrKeyboardState::reset()
     {
         clear_input();
         caps = false;
+        symbols = false;
+        caps_locked = false;
     }
 
     void XrKeyboardState::push_char(char c)
@@ -39,6 +51,8 @@ namespace ui {
         bool use_caps = caps || Input::is_grab_pressed(HAND_RIGHT);
         input += (use_caps ? std::toupper(c) : c);
         text->set_text(input);
+        p_caret++;
+        disable_caps();
     };
 
     void XrKeyboardState::remove_char()
@@ -50,6 +64,7 @@ namespace ui {
 
         input.pop_back();
         text->set_text(input);
+        p_caret--;
     }
 
     void XrKeyboardState::toggle_caps()
@@ -59,10 +74,15 @@ namespace ui {
         if (!caps) {
             caps_locked = false;
         }
+
+        layout_dirty = true;
     }
 
     void XrKeyboardState::toggle_caps_lock()
     {
+        // since it's double click, the first click toggled it..
+        //bool is_caps = !caps;
+
         if (!caps) {
             return;
         }
@@ -75,11 +95,14 @@ namespace ui {
         else {
             caps = true;
         }
+
+        layout_dirty = true;
     }
 
     void XrKeyboardState::toggle_symbols()
     {
         symbols = !symbols;
+        layout_dirty = true;
     }
 
     void XrKeyboardState::disable_caps()
@@ -87,7 +110,13 @@ namespace ui {
         if (caps && !caps_locked) {
             Node::emit_signal("Shift@pressed", (void*)nullptr);
             caps = false;
+            layout_dirty = true;
         }
+    }
+
+    float XrKeyboardState::get_caret_position()
+    {
+        return text->text_entity->get_text_width(input.substr(0, p_caret));
     }
 
     void Keyboard::initialize()
@@ -106,88 +135,39 @@ namespace ui {
         keyboard_size = glm::vec2(button_size * (max_col_count + 3u), button_size * (max_row_count + 2u) + input_height);
         Color panel_color = Color(0.0f, 0.0f, 0.0f, 0.8f);
 
-        std::vector<XrKey> keys;
-        create_keyboard_letters_layout(keys, start_pos.x, start_pos.y + input_height, button_margin);
-
         keyboard_2d = new Node2D(name, { 0.0f, 0.0f }, { 1.0f, 1.0f });
 
-        ui::XRPanel* root = new ui::XRPanel(name + "_letters", panel_color, { 0.0f, 0.f }, keyboard_size);
-        keyboard_2d->add_child(root);
+        root_common = new ui::XRPanel(name + "_common", panel_color, { 0.0f, 0.f }, keyboard_size);
+        root_common->render_background = false;
+        keyboard_2d->add_child(root_common);
 
-        ui::XRPanel* root_symbols = new ui::XRPanel(name + "_symbols", panel_color, { 0.0f, 0.f }, keyboard_size);
-        root_symbols->set_visibility(false);
-        keyboard_2d->add_child(root_symbols);
+        root_lc = new ui::XRPanel(name + "_letters", panel_color, { 0.0f, 0.f }, keyboard_size);
+        keyboard_2d->add_child(root_lc);
+
+        root_uc = new ui::XRPanel(name + "_uppercase", panel_color, { 0.0f, 0.f }, keyboard_size);
+        root_uc->set_visibility(false);
+        keyboard_2d->add_child(root_uc);
+
+        root_sym = new ui::XRPanel(name + "_symbols", panel_color, { 0.0f, 0.f }, keyboard_size);
+        root_sym->set_visibility(false);
+        keyboard_2d->add_child(root_sym);
 
         // Input text
         ui::Container2D* title_container = new ui::Container2D(name + "_title", { 0.0f, 0.0f }, { 512.0f, 48.f }, colors::BLUE);
         keyboard_2d->add_child(title_container);
 
-        ui::Text2D* text = new ui::Text2D(":", { start_pos.x + 12.0f, start_pos.y + 8.0f }, 18.0f, ui::SKIP_TEXT_SHADOW);
-        state.text = text;
-        title_container->add_child(text);
+        input_start_position = start_pos.x + 12.0f;
+        state.text = new ui::Text2D(":", { input_start_position, start_pos.y + 8.0f }, 18.0f, ui::SKIP_TEXT_SHADOW);
+        title_container->add_child(state.text);
 
-        auto process_click = [rs = root_symbols, rl = root](const std::string& sg, void* data) {
-            if (sg == "Shift") {
-                state.toggle_caps();
-            }
-            else if (sg == "Backspace") {
-                state.remove_char();
-            }
-            else if (sg == "Space") {
-                state.push_char(' ');
-            }
-            else if (sg == "HideKeyboard") {
-                state.reset();
-                close();
-            }
-            else if (sg == "Enter") {
-                state.callback(state.get_input());
-                close();
-            }
-            else if (sg == "Symbols") {
-                state.toggle_symbols();
-                rs->set_visibility(state.symbols);
-                rl->set_visibility(!state.symbols);
-            }
-            else {
-                state.push_char(sg[0]);
-                state.disable_caps();
-            }
-        };
+        // Create caret (will blink and be moved at the desired position)
+        caret = new ui::Text2D("|", { input_start_position, start_pos.y + 8.0f }, 18.0f, ui::SKIP_TEXT_SHADOW);
+        title_container->add_child(caret);
 
-        auto process_dbl_click = [](const std::string& sg, void* data) {
-            if (sg == "Shift@dbl_click") {
-                state.toggle_caps_lock();
-            }
-        };
+        // Crete and bind events for each key
+        create_keys(start_pos.x, start_pos.y + input_height, button_margin);
 
-        auto process_long_click = [](const std::string& sg, void* data) {
-            if (sg == "Backspace@long_click") {
-                state.clear_input();
-            }
-        };
-
-        for (const XrKey& key : keys) {
-            root->add_child(new ui::TextureButton2D(key.label, key.texture_path, key.flags | ui::SKIP_NAME, key.position, key.size));
-
-            if (key.bind_event) {
-                Node::bind(key.label, process_click);
-                Node::bind(key.label + "@dbl_click", process_dbl_click);
-                Node::bind(key.label + "@long_click", process_long_click);
-            }
-        }
-
-        // Fill symbols panel
-        keys.clear();
-        create_keyboard_symbols_layout(keys, start_pos.x, start_pos.y + input_height, button_margin);
-
-        for (const XrKey& key : keys) {
-            root_symbols->add_child(new ui::TextureButton2D(key.label, key.texture_path, key.flags | ui::SKIP_NAME, key.position, key.size));
-
-            if (key.bind_event) {
-                Node::bind(key.label, process_click);
-            }
-        }
+        bind_special_keys();
 
         // Move keyboard to center it (at least in flat screen)
         auto webgpu_context = Renderer::instance->get_webgpu_context();
@@ -224,6 +204,26 @@ namespace ui {
 
         auto renderer = static_cast<RoomsRenderer*>(RoomsRenderer::instance);
 
+        // Set layout
+        if(state.layout_dirty) {
+            root_lc->set_visibility(!state.symbols && !state.caps);
+            root_uc->set_visibility(!state.symbols && state.caps);
+            root_sym->set_visibility(state.symbols);
+            state.layout_dirty = false;
+        }
+
+        // move and blink caret
+        {
+            static float timer = 0.0f;
+            timer += delta_time;
+            float x_pos = input_start_position + state.get_caret_position();
+            caret->set_translation({ x_pos - 2.0f, caret->get_local_translation().y });
+            if (timer > 0.5f) {
+                caret->set_visibility(!caret->get_visibility());
+                timer = 0.0f;
+            }
+        }
+
         if (renderer->get_openxr_available()) {
             glm::mat4x4 m(1.0f);
             glm::vec3 eye = renderer->get_camera_eye();
@@ -259,70 +259,179 @@ namespace ui {
         }
     }
 
-    void Keyboard::create_keyboard_letters_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin)
+    void Keyboard::create_keys(float start_x, float start_y, float margin)
+    {
+        // Create common keys panel
+        std::vector<XrKey> keys;
+        create_keyboard_common_layout(keys, start_x, start_y, margin);
+
+        for (const XrKey& key : keys) {
+            root_common->add_child(new ui::TextureButton2D(key.label, key.texture_path, key.flags | ui::SKIP_NAME, key.position, key.size));
+        }
+
+        // Create lower case letter panel
+        keys.clear();
+        create_keyboard_letters_layout(keys, start_x, start_y, margin);
+
+        for (const XrKey& key : keys) {
+            root_lc->add_child(new ui::TextureButton2D(key.label, key.texture_path, key.flags | ui::SKIP_NAME, key.position, key.size));
+        }
+
+        // Fill upper case letters panel
+        keys.clear();
+        create_keyboard_letters_layout(keys, start_x, start_y, margin, true);
+
+        for (const XrKey& key : keys) {
+            root_uc->add_child(new ui::TextureButton2D(key.label, key.texture_path, key.flags | ui::SKIP_NAME, key.position, key.size));
+        }
+
+        // Fill symbols panel
+        keys.clear();
+        create_keyboard_symbols_layout(keys, start_x, start_y, margin);
+
+        for (const XrKey& key : keys) {
+            root_sym->add_child(new ui::TextureButton2D(key.label, key.texture_path, key.flags | ui::SKIP_NAME, key.position, key.size));
+        }
+    }
+
+    void Keyboard::bind_special_keys()
+    {
+        // Backspace
+        {
+            Node::bind("Backspace", [&](const std::string& sg, void* data) {
+                state.remove_char();
+            });
+
+            Node::bind("Backspace@long_click", [&](const std::string& sg, void* data) {
+                state.clear_input();
+            });
+        }
+
+        // Shift
+        {
+            Node::bind("Shift", [&](const std::string& sg, void* data) {
+                state.toggle_caps();
+            });
+
+            Node::bind("Shift@dbl_click", [&](const std::string& sg, void* data) {
+                state.toggle_caps_lock();
+            });
+        }
+
+        // Enter
+        Node::bind("Enter", [&](const std::string& sg, void* data) {
+            const std::string& str = state.get_input();
+            if (str.size()) {
+                state.callback(str);
+                state.reset();
+                close();
+            }
+        });
+
+        // Symbols key
+        Node::bind("Symbols", [&](const std::string& sg, void* data) {
+            state.toggle_symbols();
+        });
+
+        // Space
+        Node::bind("Space", [&](const std::string& sg, void* data) {
+            state.push_char(' ');
+        });
+
+        // Hide Keyboard key
+        Node::bind("HideKeyboard", [&](const std::string& sg, void* data) {
+            state.reset();
+            close();
+        });
+    }
+
+    void Keyboard::create_keyboard_common_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin)
     {
         glm::vec2 button_size = glm::vec2(42.0f);
         glm::vec2 spacing = button_size + margin;
 
-        std::string first_row = "qwertyuiop";
-        for (size_t i = 0; i < first_row.size(); ++i) {
-            keys.push_back({ std::string(1, first_row[i]), glm::vec2(start_x + i * spacing.x, start_y) });
-        }
+        size_t first_row_size = 10;
+        size_t second_row_size = 9;
 
-        std::string second_row = "asdfghjkl";
-        for (size_t i = 0; i < second_row.size(); ++i) {
-            keys.push_back({ std::string(1, second_row[i]), glm::vec2(start_x + i * spacing.x + spacing.x * 0.10f, start_y + spacing.y) });
-        }
-
-        std::string third_row = "zxcvbnm,.";
-        for (size_t i = 0; i < third_row.size(); ++i) {
-            keys.push_back({ std::string(1, third_row[i]), glm::vec2(start_x + (i + 1) * spacing.x + spacing.x * 0.5f, start_y + spacing.y * 2.0f) });
-        }
-
-        // Additional rows (space, enter, backspace)
-
+        // Backspace
         keys.push_back({
             .label = "Backspace",
-            .position = glm::vec2(start_x + first_row.size() * spacing.x, start_y),
+            .position = glm::vec2(start_x + first_row_size * spacing.x, start_y),
             .flags = ui::LONG_CLICK,
             .texture_path = "data/textures/buttons/backspace_key.png"
-        });
+            });
 
+        // Shift
         keys.push_back({
             .label = "Shift",
             .position = glm::vec2(start_x + spacing.x * 0.5f, start_y + spacing.y * 2.0f),
             .flags = ui::ALLOW_TOGGLE | ui::DBL_CLICK,
             .texture_path = "data/textures/buttons/shift_key.png"
-        });
+            });
 
+        // Enter
         keys.push_back({
             .label = "Enter",
-            .position = glm::vec2(start_x + second_row.size() * spacing.x + spacing.x * 0.10f, start_y + spacing.y),
+            .position = glm::vec2(start_x + second_row_size * spacing.x + spacing.x * 0.10f, start_y + spacing.y),
             .size = { button_size.x * 2.0f, button_size.y }
-        });
+            });
 
         start_x += 42.f;
 
+        // Symbols key
         keys.push_back({
             .label = "Symbols",
             .position = glm::vec2(start_x + spacing.x, start_y + spacing.y * 3.0f),
             .flags = ui::ALLOW_TOGGLE,
             .texture_path = "data/textures/buttons/symbols_key.png"
-        });
+            });
 
         float space_key_width = spacing.x * 5.0f;
 
+        // Space
         keys.push_back({
             .label = "Space",
             .position = glm::vec2(start_x + spacing.x * 2.0f, start_y + spacing.y * 3.0f),
             .size = { space_key_width, button_size.y }
-        });
+            });
 
+        // Hide Keyboard key
         keys.push_back({
             .label = "HideKeyboard",
             .position = glm::vec2(start_x + spacing.x * 2.0f + space_key_width + margin, start_y + spacing.y * 3.0f),
             .texture_path = "data/textures/buttons/hide_keyboard_key.png"
-        });
+            });
+    }
+
+    void Keyboard::create_keyboard_letters_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin, bool upper_case)
+    {
+        glm::vec2 button_size = glm::vec2(42.0f);
+        glm::vec2 spacing = button_size + margin;
+
+        auto fn_char = [&](const std::string& sg, void* data) {
+            state.push_char(sg[0]);
+        };
+
+        std::string first_row = upper_case ? "QWERTYUIOP" : "qwertyuiop";
+        for (size_t i = 0; i < first_row.size(); ++i) {
+            const std::string& label = std::string(1, first_row[i]);
+            keys.push_back({ label, glm::vec2(start_x + i * spacing.x, start_y) });
+            Node::bind(label, fn_char);
+        }
+
+        std::string second_row = upper_case ? "ASDFGHJKL" : "asdfghjkl";
+        for (size_t i = 0; i < second_row.size(); ++i) {
+            const std::string& label = std::string(1, second_row[i]);
+            keys.push_back({ label, glm::vec2(start_x + i * spacing.x + spacing.x * 0.10f, start_y + spacing.y) });
+            Node::bind(label, fn_char);
+        }
+
+        std::string third_row = upper_case ? "ZXCVBNM,." : "zxcvbnm,.";
+        for (size_t i = 0; i < third_row.size(); ++i) {
+            const std::string& label = std::string(1, third_row[i]);
+            keys.push_back({ label, glm::vec2(start_x + (i + 1) * spacing.x + spacing.x * 0.5f, start_y + spacing.y * 2.0f) });
+            Node::bind(label, fn_char);
+        }
     }
 
     void Keyboard::create_keyboard_symbols_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin)
@@ -330,70 +439,29 @@ namespace ui {
         glm::vec2 button_size = glm::vec2(42.0f);
         glm::vec2 spacing = button_size + margin;
 
+        auto fn_char = [&](const std::string& sg, void* data) {
+            state.push_char(sg[0]);
+        };
+
         std::string first_row = "0123456789";
         for (size_t i = 0; i < first_row.size(); ++i) {
-            keys.push_back({ std::string(1, first_row[i]), glm::vec2(start_x + i * spacing.x, start_y) });
+            const std::string& label = std::string(1, first_row[i]);
+            keys.push_back({ label, glm::vec2(start_x + i * spacing.x, start_y) });
+            Node::bind(label, fn_char);
         }
 
         std::string second_row = "-_/:;()&@";
         for (size_t i = 0; i < second_row.size(); ++i) {
-            keys.push_back({ std::string(1, second_row[i]), glm::vec2(start_x + i * spacing.x + spacing.x * 0.10f, start_y + spacing.y) });
+            const std::string& label = std::string(1, second_row[i]);
+            keys.push_back({ label, glm::vec2(start_x + i * spacing.x + spacing.x * 0.10f, start_y + spacing.y) });
+            Node::bind(label, fn_char);
         }
 
         std::string third_row = "?!$<>%+*=";
         for (size_t i = 0; i < third_row.size(); ++i) {
-            keys.push_back({ std::string(1, third_row[i]), glm::vec2(start_x + (i + 1) * spacing.x + spacing.x * 0.5f, start_y + spacing.y * 2.0f) });
+            const std::string& label = std::string(1, third_row[i]);
+            keys.push_back({ label, glm::vec2(start_x + (i + 1) * spacing.x + spacing.x * 0.5f, start_y + spacing.y * 2.0f) });
+            Node::bind(label, fn_char);
         }
-
-        // Additional rows (space, enter, backspace)
-        // This buttons will be duplicated, but they won't be using any callback, it's for the sake of simplicity
-
-        keys.push_back({
-            .label = "Backspace",
-            .position = glm::vec2(start_x + first_row.size() * spacing.x, start_y),
-            .texture_path = "data/textures/buttons/backspace_key.png",
-            .bind_event = false
-        });
-
-        keys.push_back({
-            .label = "Shift",
-            .position = glm::vec2(start_x + spacing.x * 0.5f, start_y + spacing.y * 2.0f),
-            .flags = ui::DISABLED,
-            .texture_path = "data/textures/buttons/shift_key.png",
-            .bind_event = false
-        });
-
-        keys.push_back({
-            .label = "Enter",
-            .position = glm::vec2(start_x + second_row.size() * spacing.x + spacing.x * 0.10f, start_y + spacing.y),
-            .size = { button_size.x * 2.0f, button_size.y },
-            .bind_event = false
-        });
-
-        start_x += 42.f;
-
-        keys.push_back({
-            .label = "Symbols",
-            .position = glm::vec2(start_x + spacing.x, start_y + spacing.y * 3.0f),
-            .flags = ui::ALLOW_TOGGLE,
-            .texture_path = "data/textures/buttons/symbols_key.png",
-            .bind_event = false
-        });
-
-        float space_key_width = spacing.x * 5.0f;
-
-        keys.push_back({
-            .label = "Space",
-            .position = glm::vec2(start_x + spacing.x * 2.0f, start_y + spacing.y * 3.0f),
-            .size = { space_key_width, button_size.y },
-            .bind_event = false
-        });
-
-        keys.push_back({
-            .label = "HideKeyboard",
-            .position = glm::vec2(start_x + spacing.x * 2.0f + space_key_width + margin, start_y + spacing.y * 3.0f),
-            .texture_path = "data/textures/buttons/hide_keyboard_key.png",
-            .bind_event = false
-        });
     }
 }
--- src/framework/ui/keyboard.h
+++ src/framework/ui/keyboard.h
@@ -12,6 +12,7 @@ class Viewport3D;
 namespace ui {
 
     class Text2D;
+    class XRPanel;
 
     struct XrKey {
         std::string label = "";
@@ -19,13 +20,14 @@ namespace ui {
         uint32_t flags = 0;
         std::string texture_path = "";
         glm::vec2 size = glm::vec2(42.0f);
-        bool bind_event = true;
     };
 
     struct XrKeyboardState {
         bool caps = false;
         bool caps_locked = false;
+        bool layout_dirty = false;
         bool symbols = false;
+        uint32_t p_caret = 1u;
         std::string input = ":";
         ui::Text2D* text = nullptr;
         std::function<void(const std::string&)> callback;
@@ -37,6 +39,7 @@ namespace ui {
         void reset();
 
         std::string get_input() { return input.substr(1); }
+        float get_caret_position();
 
         void toggle_caps();
         void toggle_caps_lock();
@@ -47,15 +50,26 @@ namespace ui {
     class Keyboard {
 
         static glm::vec2 keyboard_size;
-
         static Node2D* keyboard_2d;
         static Viewport3D* xr_keyboard;
+        static ui::Text2D* caret;
+
+        static ui::XRPanel* root_common;
+        static ui::XRPanel* root_lc;
+        static ui::XRPanel* root_uc;
+        static ui::XRPanel* root_sym;
+
+        static float input_start_position;
         static bool active;
 
         static XrKeyboardState state;
 
-        static void create_keyboard_letters_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin);
+        static void create_keyboard_common_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin);
+        static void create_keyboard_letters_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin, bool upper_case = false);
         static void create_keyboard_symbols_layout(std::vector<XrKey>& keys, float start_x, float start_y, float margin);
+
+        static void create_keys(float start_x, float start_y, float margin);
+        static void bind_special_keys();
 
     public:
 
