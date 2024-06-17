b73ab2ba959597a8666c1637168aa4e1d758e967
PZerua <pachus.wtf@gmail.com>
2024-06-17 16:09:52 +0200

    disable msaa for render mirror pipeline

--- src/graphics/renderers/rooms_renderer.cpp
+++ src/graphics/renderers/rooms_renderer.cpp
@@ -482,7 +482,7 @@ void RoomsRenderer::init_mirror_pipeline()
         swapchain_bind_groups.push_back(webgpu_context->create_bind_group(uniforms, mirror_shader, 0));
     }
 
-    mirror_pipeline.create_render(mirror_shader, color_target, { .use_depth = false });
+    mirror_pipeline.create_render(mirror_shader, color_target, { .use_depth = false, .allow_msaa = false });
 }
 
 #endif
