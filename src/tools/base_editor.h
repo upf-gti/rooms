ff58a65b2aafc404707338d5e02d3e9eff135278
Juan S. Marquerie <jsmarquerie@gmail.com>
2025-05-19 17:13:09 +0200

    Web Build fixes

--- src/tools/base_editor.h
+++ src/tools/base_editor.h
@@ -3,9 +3,12 @@
 #include <string>
 #include <unordered_map>
 
-#if defined(XR_SUPPORT)
-#include "xr/xr_context.h"
+#if defined(OPENXR_SUPPORT)
+#include "xr/openxr/openxr_context.h"
+#elif defined(WEBXR_SUPPORT)
+#include "xr/webxr/webxr_context.h"
 #endif
+
 
 #include "framework/input_xr.h"
 
